6c2fd9f6a7f1928f3b3db001d3f0b33a2282a971
Axel Demborg <axel.demborg@gmail.com>
2023-04-02 21:29:52 +0200

    More progression

--- script.js
+++ script.js
@@ -6,33 +6,39 @@ const chordSynth = new Tone.PolySynth().toDestination();
 // Lower the volume of the chord synthesizer
 chordSynth.volume.value = -12;
 
-const notes = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
+const scale = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
 const chords = [['C3', 'E3', 'G3'], ['D3', 'F3', 'A3'], ['E3', 'G3', 'B3'], ['F3', 'A3', 'C4'], ['G3', 'B3', 'D4'], ['A3', 'C4', 'E4'], ['B3', 'D4', 'F4']];
 
-function generateJazzMelody() {
+function generateJazzMelody(chordProgression) {
     const melody = [];
-    for (let i = 0; i < 16; i++) {
-        melody.push({
-            note: notes[Math.floor(Math.random() * notes.length)],
-            duration: ['8n', '4n', '16n'][Math.floor(Math.random() * 3)]
-        });
+    const totalBars = chordProgression.length * 2;
+    for (let bar = 0; bar < totalBars; bar++) {
+        const currentChord = chordProgression[bar % chordProgression.length];
+        for (let beat = 0; beat < 4; beat++) {
+            const note = scale[Math.floor(Math.random() * scale.length)];
+            melody.push({
+                note,
+                duration: ['8n', '4n', '16n'][Math.floor(Math.random() * 3)],
+                time: `${bar}:0:${beat}`
+            });
+        }
     }
     return melody;
 }
 
 function generateJazzChords() {
     const progression = [];
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 8; i++) {
         progression.push(chords[Math.floor(Math.random() * chords.length)]);
     }
     return progression;
 }
 
 async function playJazz() {
-    const melody = generateJazzMelody();
     const chords = generateJazzChords();
+    const melody = generateJazzMelody(chords);
 
-    const loop = new Tone.Sequence((time, { note, duration }) => {
+    const loop = new Tone.Part((time, { note, duration }) => {
         melodySynth.triggerAttackRelease(note, duration, time);
     }, melody).start(0);
 
