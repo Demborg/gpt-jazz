d53092179676e73a80d3e081494d941929f90338
Axel Demborg <axel.demborg@gmail.com>
2023-04-02 21:19:31 +0200

    Improve dynamics of chrod loop

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const playButton = document.getElementById('playButton');
 const stopButton = document.getElementById('stopButton');
-const synth = new Tone.PolySynth().toDestination();
+const melodySynth = new Tone.PolySynth().toDestination();
+const chordSynth = new Tone.PolySynth().toDestination();
+
+// Lower the volume of the chord synthesizer
+chordSynth.volume.value = -12;
 
 const notes = ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
 const chords = [['C3', 'E3', 'G3'], ['D3', 'F3', 'A3'], ['E3', 'G3', 'B3'], ['F3', 'A3', 'C4'], ['G3', 'B3', 'D4'], ['A3', 'C4', 'E4'], ['B3', 'D4', 'F4']];
@@ -8,7 +12,10 @@ const chords = [['C3', 'E3', 'G3'], ['D3', 'F3', 'A3'], ['E3', 'G3', 'B3'], ['F3
 function generateJazzMelody() {
     const melody = [];
     for (let i = 0; i < 16; i++) {
-        melody.push(notes[Math.floor(Math.random() * notes.length)]);
+        melody.push({
+            note: notes[Math.floor(Math.random() * notes.length)],
+            duration: ['8n', '4n', '16n'][Math.floor(Math.random() * 3)]
+        });
     }
     return melody;
 }
@@ -25,12 +32,14 @@ async function playJazz() {
     const melody = generateJazzMelody();
     const chords = generateJazzChords();
 
-    const loop = new Tone.Sequence((time, note) => {
-        synth.triggerAttackRelease(note, '8n', time);
+    const loop = new Tone.Sequence((time, { note, duration }) => {
+        melodySynth.triggerAttackRelease(note, duration, time);
     }, melody).start(0);
 
     const chordLoop = new Tone.Sequence((time, chord) => {
-        synth.triggerAttackRelease(chord, '1m', time);
+        const duration = ['1m', '2n', '4n'][Math.floor(Math.random() * 3)];
+        const delay = Math.random() * 0.5;
+        chordSynth.triggerAttackRelease(chord, duration, time + delay);
     }, chords).start(0);
 
     Tone.Transport.bpm.value = 120;
